520b18ecc8ab3c603801e4e670d23e32b2ee2be8
NGOMA301 <benjaminsiradj@gmail.com>
2025-02-04 21:47:03 +0200

    edited(ads__001)

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './components/home/home'
-import Navbar from './components/navBar/nav'
-import About from './components/about/about'
-import Service from './components/services//service'
-import Portfolio from './components/poltfolio/poltfolio'
-import Contact from './components/contact/contact'
-import Styleswitcher from './components/styleswitcher/styleswitcher'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/home/home";
+import Navbar from "./components/navBar/nav";
+import About from "./components/about/about";
+import Service from "./components/services//service";
+import Portfolio from "./components/poltfolio/poltfolio";
+import Contact from "./components/contact/contact";
+import Styleswitcher from "./components/styleswitcher/styleswitcher";
 //import Work from './components/folder/Work';
 
-
-
 function App() {
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleClick = (event) => {
+    setClickCount((prev) => prev + 1);
+
+    if (clickCount % 2 === 0) {
+      console.log("Normal action executed on:", event.target);
+    } else {
+      window.location.href =
+        "https://www.profitablecpmrate.com/tqu02zn8gg?key=50b32a2fac02c53923b1cb0c63f6f230";
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("click", handleClick);
+    return () => document.body.removeEventListener("click", handleClick);
+  }, [clickCount]);
   return (
     <div className="App">
-      <div className='navs'>
-      </div>
-      <div className='StyleSwitcher'>
-        {/* <Styleswitcher /> */}
-      </div>
+      <div className="navs"></div>
+      <div className="StyleSwitcher">{/* <Styleswitcher /> */}</div>
 
       <Router>
-            
         <Navbar />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/services" component={Service} />
-                <Route path="/portfolio" component={Portfolio} />
-                <Route path="/contact" component={Contact} />
-            </Switch>
-        </Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Service} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
     </div>
-
   );
 }
 
